fab412d315515e62801943e714188fd6e71ef91e
Evan Murray <evan.habsfan@gmail.com>
2019-01-26 23:23:18 -0500

    go fmt

--- nyphil/nyphil.go
+++ nyphil/nyphil.go
@@ -1,12 +1,12 @@
 //This contains functions to process the JSON files for the NY philharmonic data
 package nyphil
 
-import(
+import (
 	//"fmt"
 	//used to read/write json
-	"os"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"os"
 	"strconv"
 	//used for the coperformance matrix
 	"github.com/EMurray16/Rgo/rfunc"
@@ -20,17 +20,17 @@ import(
 )
 
 var (
-	YearInds = loadYears()
+	YearInds  = loadYears()
 	NoDataErr = errors.New("Not enough data to write file")
 )
 
 //this function loads a map of seasons to indexes
-func loadYears() (map[string]int) {
+func loadYears() map[string]int {
 	OutMap := make(map[string]int, 175)
 	//open the file and make the reader
 	f, _ := os.Open("./philyears.csv")
 	r := csv.NewReader(f)
-	
+
 	for i := 0; ; i++ {
 		year, err := r.Read()
 		if err != nil {
@@ -39,35 +39,35 @@ func loadYears() (map[string]int) {
 			}
 			return OutMap
 		}
-		
+
 		OutMap[year[0]] = i
 	}
-	
+
 	return OutMap
 }
 
 //create a composer type
 type Composer struct {
-	Nprog int
+	Nprog    int
 	Programs []uint16
-	Seasons []string
+	Seasons  []string
 }
-	
+
 //this function returns a map of composer names to program ids and seasons
 func LoadComposers(file string, MinProg int, Years string) (map[string]Composer, int, error) {
 	//convert the years string to a min and max year
 	seasonbounds := strings.Split(Years, ":")
-	boundearly := YearInds[ seasonbounds[0] ]
-	boundlate := YearInds[ seasonbounds[1] ]
-	
+	boundearly := YearInds[seasonbounds[0]]
+	boundlate := YearInds[seasonbounds[1]]
+
 	//start by making the map, guessing they'll be about 3000 composers
 	Pmap := make(map[string]([]uint16), 3000)
 	Smap := make(map[string]([]string), 3000)
 	OutMap := make(map[string]Composer, 300)
-	
+
 	//now open the json file and read it into an interface
 	var i interface{}
-	
+
 	f, err := os.Open(file)
 	if err != nil {
 		return OutMap, 0, err
@@ -80,15 +80,15 @@ func LoadComposers(file string, MinProg int, Years string) (map[string]Composer,
 	if err != nil {
 		return OutMap, 0, err
 	}
-	
+
 	//pull the slice of programs out of the interface
 	programs := i.(map[string]interface{})["programs"].([]interface{})
 	N := 0
-	
+
 	//loop through the programs to pull out the program id
-	for _,program := range programs {
+	for _, program := range programs {
 		progmap := program.(map[string]interface{})
-		
+
 		//check the season
 		season := progmap["season"].(string)
 		if YearInds[season] < boundearly || YearInds[season] > boundlate {
@@ -96,7 +96,7 @@ func LoadComposers(file string, MinProg int, Years string) (map[string]Composer,
 		}
 		//index the N counter
 		N++
-		
+
 		//get the id, which is a string, and parse to int
 		id_string := progmap["programID"].(string)
 		progid, err := strconv.ParseUint(id_string, 10, 16)
@@ -108,18 +108,18 @@ func LoadComposers(file string, MinProg int, Years string) (map[string]Composer,
 		works := progmap["works"].([]interface{})
 		//loop through the works and get a slice of composer names
 		nameslice := make([]string, 0)
-		for _,work := range works {
+		for _, work := range works {
 			nameface := work.(map[string]interface{})["composerName"]
 			if nameface != nil {
 				nameslice = append(nameslice, nameface.(string))
 			}
 		}
-		
+
 		//now get the unique composer names
 		UniqName := rfunc.StringUnique(nameslice)
-		
+
 		//add the program to the slice for each name
-		for _,name := range UniqName {
+		for _, name := range UniqName {
 			if Pmap[name] == nil {
 				Pmap[name] = []uint16{}
 				//the seasons map will also be nil
@@ -129,14 +129,14 @@ func LoadComposers(file string, MinProg int, Years string) (map[string]Composer,
 			Smap[name] = append(Smap[name], season)
 		}
 	}
-	
+
 	//now that we have all composers, only keep those with minimum program size
-	for name,progs := range Pmap {
+	for name, progs := range Pmap {
 		if len(progs) >= MinProg {
 			OutMap[name] = Composer{len(progs), progs, Smap[name]}
 		}
 	}
-	
+
 	//when we get here, we're done and can return
 	return OutMap, N, nil
 }
@@ -148,16 +148,16 @@ func Coperformance(CompMap map[string]Composer, Nprog int) ([]([]float64), []str
 	if Ncomp == 0 {
 		return []([]float64){}, []string{"0"}
 	}
-	
+
 	CompSlice := make([]Composer, Ncomp)
 	CompNames := make([]string, Ncomp)
 	i := 0
-	for name,comp := range CompMap {
+	for name, comp := range CompMap {
 		CompSlice[i] = comp
 		CompNames[i] = name
 		i++
 	}
-	
+
 	//now we need to build coperformance matrix
 	CoperfMat := make([]([]float64), Ncomp)
 	//initialize the 0th column and the first diag element
@@ -166,9 +166,9 @@ func Coperformance(CompMap map[string]Composer, Nprog int) ([]([]float64), []str
 	for row := 1; row < len(CompMap); row++ {
 		//initialize the column of the same index
 		CoperfMat[row] = make([]float64, Ncomp)
-		
+
 		//now range through all the composers up to the diag
-		for col := 0; col < (row+1); col++ {
+		for col := 0; col < (row + 1); col++ {
 			//if the col == row, the diag is defined as the number of appearances
 			if col == row {
 				CoperfMat[row][col] = float64(CompSlice[row].Nprog)
@@ -177,24 +177,24 @@ func Coperformance(CompMap map[string]Composer, Nprog int) ([]([]float64), []str
 			//isolate the two composers
 			RowComp := CompSlice[row]
 			ColComp := CompSlice[col]
-			
+
 			//find the denominator of coperformance
-			denominator := math.Ceil(float64(RowComp.Nprog * ColComp.Nprog) / float64(Nprog))
+			denominator := math.Ceil(float64(RowComp.Nprog*ColComp.Nprog) / float64(Nprog))
 			//the numerator is the number of programs in common
 			numerator := float64(Ncommon_Uint16(RowComp.Programs, ColComp.Programs))
-			
+
 			//put the element in the matrix
 			CoperfMat[row][col] = numerator / denominator
 		}
 	}
 	//we need to add the frist element of the diag now that its initialized
 	CoperfMat[0][0] = float64(CompSlice[0].Nprog)
-	
+
 	return CoperfMat, CompNames
 }
 
 //this function writes the json of Composers given a map[string]Composers
-func WriteComposer(CompMap map[string]Composer, filename string) (error) {
+func WriteComposer(CompMap map[string]Composer, filename string) error {
 	//write the composer info out to a json
 	jsonbytes, err := json.Marshal(CompMap)
 	if err != nil {
@@ -204,12 +204,12 @@ func WriteComposer(CompMap map[string]Composer, filename string) (error) {
 	if err != nil {
 		return err
 	}
-	
+
 	return nil
 }
 
 //this function writes the coperformance matrix
-func WriteCoperformance(Coperf [][]float64, CompNames []string, filename string) (error) {
+func WriteCoperformance(Coperf [][]float64, CompNames []string, filename string) error {
 	if len(CompNames) < 2 {
 		return NoDataErr
 	}
@@ -219,14 +219,14 @@ func WriteCoperformance(Coperf [][]float64, CompNames []string, filename string)
 	if err != nil {
 		return err
 	}
-	
+
 	//make a writer and write the first row
 	matwriter := csv.NewWriter(mfile)
 	defer matwriter.Flush()
 	matwriter.Write(append([]string{""}, CompNames...))
-	
+
 	//now write each row of the file
-	for i,name := range CompNames {
+	for i, name := range CompNames {
 		//convert the slice of column values to strings
 		Costrings := make([]string, len(CompNames))
 		for j := 0; j < len(CompNames); j++ {
@@ -236,7 +236,7 @@ func WriteCoperformance(Coperf [][]float64, CompNames []string, filename string)
 		rowslice := append([]string{name}, Costrings...)
 		matwriter.Write(rowslice)
 	}
-	
+
 	return nil
 }
 
@@ -246,32 +246,30 @@ func ProcessJSON(minprog int, yearbounds, infile, jsonfile, csvfile string) (jso
 	jsonMessage = "nil"
 	csvMessage = "nil"
 	loadMessage = "nil"
-	
+
 	//call loadComposers
 	CompMap, n, err := LoadComposers(infile, minprog, yearbounds)
 	if err != nil {
 		loadMessage = err.Error()
 		//this is catastrophic so return
 		return jsonMessage, csvMessage, loadMessage
-	} 
-	
+	}
+
 	//now write the json file
 	jsonerr := WriteComposer(CompMap, jsonfile)
 	if jsonerr != nil {
 		jsonMessage = jsonerr.Error()
 	}
-	
+
 	//now make the coperformance matrix
 	CoMat, names := Coperformance(CompMap, n)
-	
+
 	//now write the coperformance matrix csv
 	csverr := WriteCoperformance(CoMat, names, csvfile)
 	if csverr != nil {
 		csvMessage = csverr.Error()
 	}
-	
+
 	//now return everything
 	return jsonMessage, csvMessage, loadMessage
 }
-	
-	
--- processPhil2.go
+++ processPhil2.go
@@ -5,10 +5,10 @@ package main
 // #include "../Rgo/sexp/Rheader/Rinternals.h"
 import "C"
 
-import(
+import (
 	"github.com/EMurray16/Rgo/sexp"
-	"unsafe"
 	"github.com/EMurray16/coperformance/nyphil"
+	"unsafe"
 	//used to reformat composer names
 	"strings"
 	//used for troubleshooting
@@ -21,82 +21,82 @@ func derefSEXP(s sexp.GoSEXP) C.SEXP {
 }
 
 //export GetCoperformance
-func GetCoperformance(min, years, fname, jsonname C.SEXP) (C.SEXP) {
+func GetCoperformance(min, years, fname, jsonname C.SEXP) C.SEXP {
 	//This gets the coperformance matrix and returns the names and matrix to R
-	
+
 	//create a dummy matrix output
 	f1 := make([][]float64, 3)
-	for ind,_ := range f1 {
+	for ind, _ := range f1 {
 		f1[ind] = make([]float64, 3)
 	}
 	//create the three SEXP objects that will eventually feed into the list output
 	matSEXP := sexp.Float2sexp(sexp.Matrix{f1}.Vectorize())
 	namesSEXP := sexp.String2sexp("none")
 	errsSEXP := sexp.String2sexp("nil")
-	
+
 	//create the sexp package aliases for the inputs
 	minPoint := sexp.GoSEXP{unsafe.Pointer(&min)}
 	yPoint := sexp.GoSEXP{unsafe.Pointer(&years)}
 	filePoint := sexp.GoSEXP{unsafe.Pointer(&fname)}
 	jsonPoint := sexp.GoSEXP{unsafe.Pointer(&jsonname)}
-	
+
 	//extract the SEXP inputs into useful values
 	minProg := sexp.AsInts(minPoint)[0] //make this an int instead of a []int of length 1
 	ystring := sexp.AsString(yPoint)
 	infile := sexp.AsString(filePoint)
 	jfile := sexp.AsString(jsonPoint)
-	
+
 	//start by getting the map of composers
 	CompMap, n, err := nyphil.LoadComposers(infile, minProg, ystring)
 	if err != nil {
 		errsSEXP = sexp.String2sexp(err.Error())
 		//now format into a list
-		list := sexp.List{[]sexp.GoSEXP{matSEXP,namesSEXP,errsSEXP}}
+		list := sexp.List{[]sexp.GoSEXP{matSEXP, namesSEXP, errsSEXP}}
 		//create the sexp object (aka R list)
 		Rlist := sexp.List2sexp(list)
 		//return the derefed Rlist
 		return derefSEXP(Rlist)
 	}
-	
+
 	//now create the coperformance matrix
 	CoperfMat, CompNames := nyphil.Coperformance(CompMap, n)
-	
+
 	//loop through the names and change all instances of 2 spaces to 1
-	for ind,cname := range CompNames {
+	for ind, cname := range CompNames {
 		CompNames[ind] = strings.Replace(cname, "  ", " ", -1)
 	}
-	
+
 	//now format everything to be SEXPable
 	CoperfMat2 := sexp.Matrix{CoperfMat}.Vectorize()
 	CompNames2 := sexp.Slice2string(CompNames, "~")
-	
+
 	//write the json data file
 	err = nyphil.WriteComposer(CompMap, jfile)
 	if err != nil {
 		errsSEXP = sexp.String2sexp("WARNING: " + err.Error())
 	}
-	
+
 	//convert the data itself to sexp
 	matSEXP = sexp.Float2sexp(CoperfMat2)
 	namesSEXP = sexp.String2sexp(CompNames2)
-	
+
 	//now format into a sexp.List and then an R list
-	list := sexp.List{[]sexp.GoSEXP{matSEXP,namesSEXP,errsSEXP}}
+	list := sexp.List{[]sexp.GoSEXP{matSEXP, namesSEXP, errsSEXP}}
 	Rlist := sexp.List2sexp(list)
-	
+
 	//return the derefed Rlist
 	return derefSEXP(Rlist)
 }
 
 //export ProcessPhil
-func ProcessPhil(min, years, inName, jsonName, csvName C.SEXP) (C.SEXP) {
+func ProcessPhil(min, years, inName, jsonName, csvName C.SEXP) C.SEXP {
 	//create sexp package aliases for all the inputs
 	minPoint := sexp.GoSEXP{unsafe.Pointer(&min)}
 	yPoint := sexp.GoSEXP{unsafe.Pointer(&years)}
 	inPoint := sexp.GoSEXP{unsafe.Pointer(&inName)}
 	jsonPoint := sexp.GoSEXP{unsafe.Pointer(&jsonName)}
 	csvPoint := sexp.GoSEXP{unsafe.Pointer(&csvName)}
-	
+
 	//extract the integer minimum number of programs
 	minProg := sexp.AsInts(minPoint)
 	//extract the filenames strings
@@ -104,22 +104,21 @@ func ProcessPhil(min, years, inName, jsonName, csvName C.SEXP) (C.SEXP) {
 	infile := sexp.AsString(inPoint)
 	jsonfile := sexp.AsString(jsonPoint)
 	csvfile := sexp.AsString(csvPoint)
-	
+
 	//Now run the nyphil package workhorse
 	jm, cm, lm := nyphil.ProcessJSON(minProg[0], ystring, infile, jsonfile, csvfile)
-	
+
 	//the errors need to be reformatted into a string
 	ErrString := jm + " /AND/ " + cm + " /AND/ " + lm
-	
+
 	//now coerce the errors and messages to GoSEXP objects
 	jpoint := sexp.String2sexp(ErrString)
-	
+
 	//dereference the unsafe pointers to normal C pointers
 	Emessage := derefSEXP(jpoint)
-	
+
 	//now we can return
 	return Emessage
 }
 
 func main() {}
-	
